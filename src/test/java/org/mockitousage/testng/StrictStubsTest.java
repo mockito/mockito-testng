f6a2905c5e8a9356d5b84c80ebd3a723a63a8633
Szczepan Faber <sfaber@linkedin.com>
2018-08-18 21:59:08 -0700

    Added comprehensive coverage for strict stubbing in Mockito TestNG

--- src/test/java/org/mockitousage/testng/StrictStubsTest.java
+++ src/test/java/org/mockitousage/testng/StrictStubsTest.java
@@ -7,12 +7,17 @@ package org.mockitousage.testng;
 import org.assertj.core.api.Assertions;
 import org.mockito.Mock;
 import org.mockito.exceptions.misusing.PotentialStubbingProblem;
+import org.mockito.exceptions.misusing.UnnecessaryStubbingException;
 import org.mockito.testng.MockitoTestNGListener;
+import org.mockitousage.testng.failuretests.HasUnusedStubs;
+import org.mockitousage.testng.utils.TestNGRunner;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 @Listeners(MockitoTestNGListener.class)
@@ -24,5 +29,22 @@ public class StrictStubsTest {
         when(list.add("a")).thenReturn(true);
 
         Assertions.assertThatThrownBy(() -> list.add("b")).isInstanceOf(PotentialStubbingProblem.class);
+    }
+
+    @Test public void detects_unused_stubs() {
+        //when
+        TestNGRunner.Result result = new TestNGRunner().run(HasUnusedStubs.class);
+
+        //then
+        assertThat(result.getFailure()).isInstanceOf(UnnecessaryStubbingException.class);
+    }
+
+    @Test public void keeps_tests_dry() {
+        //when
+        when(list.add("a")).thenReturn(true);
+        list.add("a");
+
+        //then used stubbing is implicitly verified
+        verifyNoMoreInteractions(list);
     }
 }
